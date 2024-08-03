a4304d3c2e9aebbd062964e203c62664291059ec
Marley <marley.nodejs@gmail.com>
2024-08-03 20:20:41 +0300

    feat: Create your new website using Angular 17 - Part 1 -- Transforming HTML into Angular 17 Magic!

--- Readme.md
+++ Readme.md
@@ -6,10 +6,27 @@ https://www.youtube.com/watch?v=Lyd9tcttXDA&list=PLY7EDLM5qnl7dcJZj8-g4h9mfvTt3i
 **Original src:**  
 https://github.com/admister/Angular-17-website/
 
-
 **Demo**  
 https://demos.onepagelove.com/html/namari/
 
-
 **Download Template**  
 https://onepagelove.com/namari
+
+<hr/>
+
+### Create your new website using Angular 17 - Part 1 -- Transforming HTML into Angular 17 Magic!
+
+<br/>
+
+```
+$ cd prj
+$ ng generate component /components/header
+$ ng generate component /components/nav
+$ ng generate component /components/about
+$ ng generate component /components/gallery
+$ ng generate component /components/services
+$ ng generate component /components/testimonials
+$ ng generate component /components/clients
+$ ng generate component /components/pricing
+$ ng generate component /components/footer
+```
--- prj/src/app/app.component.ts
+++ prj/src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    AboutComponent,
+    ClientsComponent,
+    PricingComponent,
+    TestimonialsComponent,
+    GalleryComponent,
+    NavComponent,
+    ServicesComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app';
